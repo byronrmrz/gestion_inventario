6f686e332c6523ff7a2b52ea08e53b1cf80a0618
Byron Ramírez <87165998+byronrmrz@users.noreply.github.com>
2025-04-08 21:44:32 -0600

    adding video explication

--- frontend/features/product/productApi.ts
+++
@@ -1,7 +0,0 @@
-export const fetchProducts = async () => {
-    const response = await fetch('http://localhost:3001/getProducts');
-    if (!response.ok) {
-        throw new Error('Failed to fetch products');
-    }
-    return response.json();
-}
